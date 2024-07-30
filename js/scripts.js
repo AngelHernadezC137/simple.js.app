1f21af946c5c5d5d0a53e446138eda20d3fc5286
AngelHernadezC137 <angelinfinite9@gmail.com>
2024-07-30 16:38:06 -0700

    Updated js
    Created addListItem() function and foreach() loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
 
-let pokemonList= [
+let repository = [ 
     {  name: 'Dragonite',
        height: 7, 
        type:['Dragon','Flying']},
@@ -12,25 +12,52 @@ let pokemonList= [
     {  name: 'Giritina', 
        height: 8, 
        type:['Dragon','Ghost']},
-];
-
-function getAll() {
-   return pokemonList;
-}
-
+      ];
 function add(pokemon) {
-   pokemonList.push(pokemon);
+   if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+   ) {
+      repository.push(pokemon);
+   } else {
+      console.log("pokemon is not correct");
+   }
+}
+function getAll() {
+   return repository;
+ }
+ function addListItem(pokemon){
+   let pokemonList = document.querySelector(".pokemon-list");
+   let listPokemon = document.createElement("li");
+   let button = document.createElement("button");
+   button.innerText = pokemon.name;
+   button.classList.add("button-class");
+   listPokemon.appendChild(button);
+   pokemonList.appendChild(listPokemon);
+   listPokemon.addEventListener('click', function (event) {
+      showDetails(pokemon);
+    });
+    
+ }
+
+function showDetails(pokemon) {
+   console.log(pokemon);
 }
 
-return {
-   getAll,
-   add
+ return {
+   add: add,
+   getAll: getAll,
+   addListItem: addListItem,
+   showDetails: showDetails,
 };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-   document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
-   if (pokemon.height > 6) {
-       document.write ('  - Wow, that’s big!');
-   }
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(function (pokemon) {
+ pokemonRepository.addListItem(pokemon);
+
 });
